e0467435a8254e56a3cd859ce230165d5495d32d
Mathias Ooi <mathias7142@gmail.com>
2021-07-09 23:40:33 -0700

    Card animations & logic for game

--- script.js
+++ script.js
@@ -1,20 +1,81 @@
-var selected = [false, false, false, false, false, false, false, false, false, false, false, false];
+var selected = [];
 var deck = [];
 var color = ["red", "green", "purple"];
 var pattern = ["filled", "dashed", "empty"];
 var shape = ["squiggle", "oval", "diamond"];
 
-function cardClicked(card) {
-    selected[card] = !selected[card];
-    console.log(card);
-    if (selected[card])
-        document.getElementById("c" + String(card)).classList.add("card-td-selected");
-    else
-        document.getElementById("c" + String(card)).classList.remove("card-td-selected");
+function remove(arr) {
+    // Stupid js doesnt have remove by value
+    // python better
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+}
 
+function cardClicked(card) {
+    // Add card to selected or remove if its already inside & change box shadow
+    console.log(card);
+    if (selected.includes(card)) {
+        remove(selected, card);
+        document.getElementById("c" + String(card)).classList.remove("card-td-selected");
+        return;
+    }
+    selected.push(card);
+    document.getElementById("c" + String(card)).classList.add("card-td-selected");
+
+    // Check if the selected are a set and then unselect them
+    if (selected.length == 3) {
+        for (var i = 0; i < 3; ++i) {
+            var elm = document.getElementById("c" + String(selected[i]));
+            elm.classList.remove("card-td-correct");
+            elm.classList.remove("card-td-incorrect");
+            elm.classList.remove("card-td-selected");
+            var newone = elm.cloneNode(true);
+            elm.parentNode.replaceChild(newone, elm);
+        }
+        if (isSet()) {
+            console.log("IS SET", selected);
+            for (var i = 0; i < 3; ++i) {
+                document.getElementById("c" + String(selected[i])).classList.add("card-td-correct");
+            }
+            // Remove cards if is set
+            console.log(deck);
+            toRemove = [];
+            for (var i = 0; i < 3; ++i) {
+                i = selected[i];
+                [deck[i], deck[i + 12]] = [deck[i + 12], deck[i]];
+                toRemove.push(deck[i + 12]);
+            }
+            remove(deck, toRemove[0], toRemove[1], toRemove[2]);
+            display();
+        }
+        else {
+            console.log("NOT SET", selected);
+            for (var i = 0; i < 3; ++i) {
+                document.getElementById("c" + String(selected[i])).classList.add("card-td-incorrect");
+            }
+        }
+        selected = [];
+    }
+}
+
+function isSet() {
+    console.log(selected);
+    var a = deck[selected[0]].members(), b = deck[selected[1]].members(), c = deck[selected[2]].members();
+    for (var i = 0; i < 4; ++i) {
+        if (!((a[i] != b[i] && a[i] != c[i] && b[i] != c[i]) || (a[i] == b[i] && a[i] == c[i]))) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function loadDeck() {
+    deck = []
     for (let i = 1; i < 4; ++i) {
         for (let j = 0; j < color.length; ++j) {
             for (let k = 0; k < pattern.length; ++k) {
@@ -25,7 +86,16 @@ function loadDeck() {
             }
         }
     }
-    console.log("Loaded Deck:", deck);
+}
+
+function shuffle() {
+    // Fisher-Yates shuffle
+    var i = deck.length, k;
+    while (i !== 0) {
+        k = Math.floor(Math.random() * i);
+        i--;
+        [deck[i], deck[k]] = [deck[k], deck[i]];
+    }
 }
 
 class Card {
@@ -36,8 +106,26 @@ class Card {
         this.color = color;
     }
 
+    members() {
+        return [this.num, this.shape, this.pattern, this.color];
+    }
+
     svg() {
         // bruh svgs are hard how to do them help
-        return this.num, this.shape, this.pattern, this.color;
+        return String(this.num + " " + this.shape + " " + this.pattern + " " + this.color);  // Gonna stay with this until i feel like doing svgs :(
     }
+}
+
+function display() {
+    for (var i = 0; i < 12; ++i) {
+        document.getElementById("c" + i).innerHTML = deck[i].svg();
+    }
+}
+
+function main() {
+    loadDeck();
+    console.log("Loaded deck", deck);
+    // shuffle();
+    // console.log("Shuffled deck", deck);
+    display();
 }
